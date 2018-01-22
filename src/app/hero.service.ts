1bc3a7a7e82c09a615eb5271d3b4d9930916dcfa
Zac Bell <bellz@byui.edu>
2018-01-22 15:50:46 -0700

    test

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,12 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+  
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
